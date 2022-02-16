c0852ce50cff4b02218a449a60c7cdcb0797ab36
Aleksandery\ Tysklind <info@aleksandertysklind.com>
2022-02-16 17:28:15 +0100

    fucked up

--- assets/application.js
+++ assets/application.js
@@ -1,4 +1,12 @@
 // Put your applicaiton javascript here
-$(document.ready(function () {
-    
+$(document).ready(function() {
+
+  $(document).on('change', '#variant-id', function() {
+    let
+      variant_id = this.value,
+      variant_image = $('.single-product-image[data-variant="'+variant_id+'"]');
+
+    variant_image.show().siblings(':visible').hide();
+  })
+
 });
